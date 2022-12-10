bfaf7caecb9414ff4eed012a96d0dc1e65fe9bb0
Lane McMartin <37342668+LaneMcMartin@users.noreply.github.com>
2022-12-10 16:44:43 -0700

    Refactored player script and project organization

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,31 +5,31 @@ extends CharacterBody2D
 @export var dash_multiplier = 3
 
 # OnReady
-@onready var dash_cooldown_timer = get_node("DashCooldownTimer")
-@onready var dash_active_timer = get_node("DashActiveTimer")
-@onready var dash_sound = get_node("DashSound")
 @onready var bubble = preload("res://Scenes/bubble.tscn")
 
-# Public
+# Movement
 var dash_available = true
 var dash_active = false
+var aim_vector
+var mouse_position
+
+# Stats
 var is_invincible = false
 var is_dead = false
 var is_raising = false
-var aim_vector
-var mouse_position
 var health = 3
 
 func _ready():
-	dash_cooldown_timer.timeout.connect(_on_dash_cooldown_timer_timeout)
-	dash_active_timer.timeout.connect(_on_dash_active_timer_timeout)
+	# Connect timers
+	$DashCooldownTimer.timeout.connect(_on_dash_cooldown_timer_timeout)
+	$DashActiveTimer.timeout.connect(_on_dash_active_timer_timeout)
 	$InvincibleTimer.timeout.connect(_on_invincible_timer_timeout)
 
 func _physics_process(delta):
 	# Check aliveness
 	if is_dead:
 		return
-		
+	
 	# Handle mouselook and aim vector
 	mouse_position = get_global_mouse_position()
 	aim_vector = (mouse_position - position).normalized()
@@ -47,48 +47,41 @@ func _physics_process(delta):
 	
 	# Handle dash
 	if Input.is_action_just_pressed("Space") and dash_available:
-		dash_available = false
-		dash_active = true
-		dash_cooldown_timer.start()
-		dash_active_timer.start()
-		dash_sound.play()
-		print("Dash timer started!")
+		dash()
+	
+	# Process movement
 	if dash_active:
 		velocity *= dash_multiplier
-	
-	# Handle raise
-	if Input.is_action_pressed("FireSecondary"):
-		raise()
-		
-	
 	move_and_slide()
 
 func damage(damage_taken):
+	# Check invincibility
 	if not is_invincible:
-		# Deduct HP
+		# Deduct HP and/or cause death
 		health -= damage_taken
-		# Check death
 		if health <= 0:
 			death()
 		# Trigger invincibility and blinking
 		$InvincibleTimer.start()
 		$AnimationPlayer.play("Flash")
-
-func _on_dash_cooldown_timer_timeout():
-	dash_available = true
-	print("Dash cooldown reset!")
-	
-func _on_dash_active_timer_timeout():
-	dash_active = false
-	print("Dash active reset!")
-	
-func _on_invincible_timer_timeout():
-	is_invincible = false
-	$AnimationPlayer.play("NoFlash")
 	
 func death():
 	Gamemanager.game_over()
 	is_dead = true
 	
-func raise():
-	pass
+func dash():
+	dash_available = false
+	dash_active = true
+	$DashCooldownTimer.start()
+	$DashActiveTimer.start()
+	$DashSound.play()
+	
+func _on_dash_cooldown_timer_timeout():
+	dash_available = true
+	
+func _on_dash_active_timer_timeout():
+	dash_active = false
+	
+func _on_invincible_timer_timeout():
+	is_invincible = false
+	$AnimationPlayer.play("NoFlash")
